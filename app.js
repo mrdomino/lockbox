7ce17c965d424a64c91dbfc216dd281902a2be92
Steve Dee <steve@smartercode.net>
2012-01-01 16:33:21 +0100

    add basic application

---
+++ app.js
@@ -0,0 +1,43 @@
+goog.provide('app');
+
+goog.require('ssss');
+goog.require('goog.array');
+goog.require('goog.crypt.base64');
+goog.require('goog.debug.ErrorHandler');
+goog.require('goog.dom');
+goog.require('goog.events');
+goog.require('goog.events.EventHandler');
+goog.require('goog.events.EventTarget');
+goog.require('goog.events.EventType');
+
+
+Uint8Array.prototype.toString = function() {
+  return goog.crypt.base64.encodeByteArray(this);
+}
+
+app.parseUint8Array = function(str) {
+  return new Uint8Array(goog.crypt.base64.decodeStringToByteArray(str));
+}
+
+app.run = function() {
+  var $ = goog.dom.getElement;
+  goog.events.listen($('split'), goog.events.EventType.CLICK, function(e) {
+    var k = parseInt($('k').value, 10);
+    var n = parseInt($('n').value, 10);
+    var input = $('input').value;
+
+    var output = ssss.split(input, k, n);
+    goog.dom.removeChildren($('split-output'));
+    goog.array.forEach(output, function(key) {
+      var child = goog.dom.createDom('pre');
+      goog.dom.setTextContent(child, key);
+      goog.dom.appendChild($('split-output'), child);
+    });
+  });
+  goog.events.listen($('combine'), goog.events.EventType.CLICK, function(e) {
+    var keys = goog.array.map($('keys').value.split("\n"), app.parseUint8Array);
+    goog.dom.setTextContent($('combine-output'), goog.crypt.base64.decodeString(ssss.combine(keys).toString()));
+  });
+}
+
+goog.exportSymbol('app.run', app.run);
--- makefile
+++ makefile
@@ -4,9 +4,9 @@ CLOSURE_BUILD=$(CLOSURE_LIBRARY)/closure/bin/build
 CLOSUREBUILDER=$(CLOSURE_BUILD)/closurebuilder.py
 DEPSWRITER=$(CLOSURE_BUILD)/depswriter.py
 PREFIX=../../../lockbox
-JS=gf28.js ssss.js comb.js
+JS=app.js gf28.js ssss.js comb.js
 
-all: comb-compiled.js gf28-compiled.js ssss-compiled.js deps.js
+all: app-compiled.js comb-compiled.js gf28-compiled.js ssss-compiled.js deps.js
 
 %-compiled.js: %.js
 	$(CLOSUREBUILDER) --root=$(CLOSURE_LIBRARY) --root=. --namespace=$* --output_mode=compiled --compiler_jar=$(CLOSURE_COMPILER_JAR) --compiler_flags=--compilation_level=ADVANCED_OPTIMIZATIONS --compiler_flags=--warning_level=VERBOSE --compiler_flags=--output_wrapper="(function(){%output%})()" > $@
