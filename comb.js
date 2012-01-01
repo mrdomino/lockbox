db14126211a3d47eba72c048e27297aa24d04ae2
Steve Dee <steve@smartercode.net>
2012-01-01 09:51:38 +0100

    combinations working

--- comb.js
+++ comb.js
@@ -1,9 +1,24 @@
 goog.provide('comb');
 
+goog.require('goog.array');
+
 
 comb.combinations = function(arr, r) {
-  if (r == 0) {
+  if (r == 0 || goog.array.isEmpty(arr)) {
     return [];
   }
-  return [arr];
+
+  var symbol = arr[0];
+  var arr_without_symbol = goog.array.slice(arr, 1);
+
+  if (r == 1) {
+    return goog.array.concat([[symbol]], comb.combinations(arr_without_symbol, 1));
+  }
+
+  var combs_without_symbol = comb.combinations(arr_without_symbol, r);
+  var combs_with_symbol = comb.combinations(arr_without_symbol, r - 1);
+  for (var i = 0; i < combs_with_symbol.length; ++i) {
+    combs_with_symbol[i] = goog.array.concat([symbol], combs_with_symbol[i]);
+  }
+  return goog.array.concat(combs_without_symbol, combs_with_symbol);
 }
