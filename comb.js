f6d375c9018e70ec7be6fc255aaf6069a67e8604
Steve Dee <steve@smartercode.net>
2012-01-02 19:12:29 -0800

    Added comb.combinationSuchThat.

--- comb.js
+++ comb.js
@@ -22,4 +22,26 @@ comb.combinations = function(arr, r) {
   return goog.array.concat(combs_without_symbol, combs_with_symbol);
 }
 
+comb.combinationSuchThat = function(pred, arr, r, opt_accum) {
+  var accum = (typeof opt_accum == 'undefined') ? [] : opt_accum;
+  if (r == 0) {
+    return pred(accum) ? accum : undefined;
+  }
+  if (goog.array.isEmpty(arr)) {
+    return undefined;
+  }
+
+  var symbol = arr[0];
+  var arr_without_symbol = goog.array.slice(arr, 1);
+
+  var candidate = comb.combinationSuchThat(pred, arr_without_symbol, r, accum);
+  if (typeof candidate != 'undefined') {
+    return candidate;
+  }
+
+  return comb.combinationSuchThat(pred, arr_without_symbol, r - 1,
+                                  goog.array.concat(accum, [symbol]));
+}
+
 goog.exportSymbol("comb.combinations", comb.combinations);
+goog.exportSymbol("comb.combinationSuchThat", comb.combinationSuchThat);
